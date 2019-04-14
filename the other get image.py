5ee4fe61c68cfe46ca4d6dc932fb48c799850551
Walter Brown <zinecarton@gmail.com>
2019-04-14 16:01:48 -0400

    added max_images option, resize on images, and return statement

--- the other get image.py
+++ the other get image.py
@@ -2,23 +2,30 @@ from PIL import Image, ImageFilter
 
 
 
-def image_data(path):
+def image_data(path, dims=(128,128), max_images=-1):
 
     import os
     labels =[]
     values =[]
+    i=0
     for filename in os.listdir(path):
         for filename2 in os.listdir(path +"/"+filename):
             labels.append(filename)
             im = Image.open(path + "/" + "/" +filename + "/" + filename2)
-            pixels = list(im.getdata())
+            pixels = list(im.resize(dims).getdata())
             values.append(pixels)
+            i+=1
+            if max_images > -1 and i > max_images:
+                break
+        if max_images > -1 and i > max_images:
+            break
         print(filename)
     data_base = (labels,values)
 
     print(data_base)
+    return data_base
 
-image_data("PeopleArt/JPEGImages")
+images, labels = image_data("PeopleArt/JPEGImages", max_images=10)
 
 # def image_to_data_set(path):
 #     im = Image.open('PeopleArt\JPEGImages\Academicism')
