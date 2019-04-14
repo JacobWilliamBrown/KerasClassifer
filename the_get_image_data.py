5dde93fe155ed49460243a2eea27d6d3a9eecf5c
Walter Brown <zinecarton@gmail.com>
2019-04-14 12:19:54 -0400

    now returns 2 arrays in a tuple

--- the_get_image_data.py
+++ the_get_image_data.py
@@ -1,11 +1,16 @@
 def image_data(path):
-
+    images = []
+    labels = []
     import os
 
-    for filename in os.listdir(path):
-        print(filename)
-
-    # import PIL
-    # next_image = Image.open(path)
-    # next_image.show()
-image_data("PeopleArt/JPEGImages")
+    for label_string in os.listdir(path):
+        for file_name in os.listdir(path+"/"+label_string):
+            from PIL import Image
+            next_image = Image.open(path+"/"+label_string+"/"+file_name)
+            images.append(next_image)
+            labels.append(label_string)
+            # next_image.show()
+    return (images, labels)
+ima, lab = image_data("PeopleArt/JPEGImages")
+ima[0].show()
+print(lab[0])
